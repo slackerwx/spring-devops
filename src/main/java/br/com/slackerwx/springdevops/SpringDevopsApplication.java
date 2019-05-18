5d8c1cb2eafab91f1eb84d935bd88afd04a0df81
Danilo Fernando Rodrigues Gomes <danilo.gomes@semantix.com.br>
2019-05-18 15:26:24 -0300

    Initial commit

---
+++ src/main/java/br/com/slackerwx/springdevops/SpringDevopsApplication.java
@@ -0,0 +1,13 @@
+package br.com.slackerwx.springdevops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDevopsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDevopsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/slackerwx/springdevops/SpringDevopsApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.slackerwx.springdevops;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringDevopsApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
